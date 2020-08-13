f6394f2afe9abe4ab4c26d4bd0a9a0cd2ce1010e
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-13 17:25:49 -0400

    Fix log issue

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Run `git submodule update --init --recursive` if the repository isn't cloned 
 recursively to begin with.
+
+To update the submodule to the latest version:
+- `cd components/esp8266-sample-component`
+- `git checkout master`
+- `git pull`
+- `git submodule update`
+- `cd ../..`
--- main/main.c
+++ main/main.c
@@ -2,12 +2,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "esp_log.h"
+
 #include "simple_sample.h"
 
 void app_main(void)
 {
-    printf_sample();
-    while (1) {
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+  ESP_LOGI("main", "This is app_main");
+  printout_sample();
+  while (1) {
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
 }
